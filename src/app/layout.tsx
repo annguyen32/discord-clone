94ec6c03236fb8cb9a466856a12efa9f58a182d3
annguyen32 <annnguyen32@gmail.com>
2024-10-29 19:38:02 -0500

    convex added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-
+import { ConvexClientProvider } from "./convex-client-provider";
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -25,10 +25,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body>
+        <ConvexClientProvider>{children}</ConvexClientProvider>
       </body>
     </html>
   );
